d9c9124f95a0b593341245af7ebb08b77a50a744
José Sousa <jose.sousa@xpand-it.com>
2018-02-14 22:53:31 +0000

    Updated hero-detail component and added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// #TODO - import heroes-detail component
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 // #TODO - import heroes component
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  // #TODO - add heroes-detail routes
+  { path: 'detail/:id', component: HeroDetailComponent },
   // #TODO - add heroes routes
   { path: '**', redirectTo: '/dashboard' }  
 ];
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,5 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
-  // #TODO - save() method
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
